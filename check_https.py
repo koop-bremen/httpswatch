e18944b20d6f02348d0d95b92ca79e23a809bfd2
Hendrik Simon <simon@koop-bremen.de>
2015-01-21 15:19:52 +0100

    Changed user agent

--- check_https.py
+++ check_https.py
@@ -19,7 +19,7 @@ from urllib.parse import urlsplit
 import jinja2
 
 PARALLELISM = 16
-USER_AGENT = "HTTPSWatch Bot (https://httpswatch.com)"
+USER_AGENT = "Mozilla/5.0 (X11; Linux i686; rv:10.0) Gecko/20100101 Firefox/10.0"
 
 log = logging.getLogger("check_https")
 
